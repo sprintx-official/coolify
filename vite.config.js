185ca32dd823cb9c508816eb97c89a724c429412
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2025-06-18 09:19:33 -0400

    feat(core): set custom API rate limit (#5984)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
 
